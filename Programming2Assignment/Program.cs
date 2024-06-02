50098ba46b95dd83eb9ef3797f9903e7e27b65ac
hydi-official <esseldacosta00@gmail.com>
2024-06-02 00:41:04 +0000

    Grade Calculator

---
+++ Programming2Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GradeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your grade (0-100):");
+            int grade = Convert.ToInt32(Console.ReadLine());
+
+            char letterGrade;
+
+            if (grade >= 90)
+                letterGrade = 'A';
+            else if (grade >= 80)
+                letterGrade = 'B';
+            else if (grade >= 70)
+                letterGrade = 'C';
+            else if (grade >= 60)
+                letterGrade = 'D';
+            else
+                letterGrade = 'F';
+
+            Console.WriteLine($"Your letter grade is: {letterGrade}");
+        }
+    }
+}
