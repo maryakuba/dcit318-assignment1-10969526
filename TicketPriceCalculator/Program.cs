b2372e01e368db85506d4265ac52dfd23702f880
hydi-official <esseldacosta00@gmail.com>
2024-06-02 00:46:03 +0000

    Ticket Price Calculator

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            int ticketPrice;
+
+            if (age <= 12 || age >= 65)
+                ticketPrice = 7;
+            else
+                ticketPrice = 10;
+
+            Console.WriteLine($"Your ticket price is GHC{ticketPrice}");
+        }
+    }
+}
